cad3280e28f2ad6c300c1c9c78eacd4d87d194a8
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-11-19 14:58:21 +0100

    add president related routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import './App.css'
 import RootLayout from './layouts/RootLayout'
-import {adminOptions} from "./layouts/side-bar-options.ts";
+import {adminOptions, presidentOptions} from "./layouts/side-bar-options.ts";
 
 function App() {
 
     const router = createBrowserRouter(
         createRoutesFromElements(
-            <Route path='/' element={<RootLayout options={adminOptions}/>}>
+            <Route path='/' element={<RootLayout options={presidentOptions}/>}>
+
                 <Route path='dashboard' element={<div>dashboard</div>}/>
                 <Route path='users' element={<div>users</div>}/>
                 <Route path='team' element={<div>team</div>}/>
                 <Route path='products' element={<div>products</div>}/>
+                <Route path='president'>
+                    <Route path='dashboard' element={<div>dashboard</div>} />
+                    <Route path='members' element={<div>members</div>} />
+                    <Route path='events' element={<div>events</div>} />
+                    <Route path='documents' element={<div>docus</div>} />
+                    <Route path='budget' element={<div>budget</div>} />
+
+
+                </Route>
             </Route>
         )
     )
