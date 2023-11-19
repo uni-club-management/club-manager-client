d328e1b9371ddf3acb26bffd2bdae78765fb754f
yassine <yassinemouddene@gmail.com>
2023-11-19 10:32:22 +0100

    made sidebar dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import './App.css'
 import RootLayout from './layouts/RootLayout'
+import {adminOptions} from "./layouts/side-bar-options.ts";
 
 function App() {
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<RootLayout/>}>
-        
-      </Route>
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path='/' element={<RootLayout options={adminOptions}/>}>
+                <Route path='dashboard' element={<div>dashboard</div>}/>
+                <Route path='users' element={<div>users</div>}/>
+                <Route path='team' element={<div>team</div>}/>
+                <Route path='products' element={<div>products</div>}/>
+            </Route>
+        )
     )
-  )
 
-  return (
-    <RouterProvider router={router}/>
-  )
+    return (
+        <RouterProvider router={router}/>
+    )
 }
 
 export default App
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,58 +1,37 @@
-import React from 'react';
-import { DesktopOutlined, FileOutlined, LaptopOutlined, NotificationOutlined, PieChartOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
+
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
-import { Outlet } from 'react-router-dom';
+import {Link, Outlet} from 'react-router-dom';
+import {SideBarOptions} from "./side-bar-options.ts";
 
 const { Content, Sider, Footer } = Layout;
 
-type MenuItem = Required<MenuProps>['items'][number];
+type Props = {
+    options: SideBarOptions[]
+}
 
-function getItem(
-    label: React.ReactNode,
-    key: React.Key,
-    icon?: React.ReactNode,
-    children?: MenuItem[],
-  ): MenuItem {
-    return {
-      key,
-      icon,
-      children,
-      label,
-    } as MenuItem;
-  }
-  
-  const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-      getItem('Tom', '3'),
-      getItem('Bill', '4'),
-      getItem('Alex', '5'),
-    ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
-  ];
-  
-type Props = {}
-
-export default function RootLayout({}: Props) {
+export default function RootLayout(props: Props) {
     const {
         token: { colorBgContainer },
       } = theme.useToken();
     
       return (
-          <Layout style={{padding: '12px', minHeight:'100vh'}}>
-            <Sider style={{width:'200', background: colorBgContainer, height:'fit-content', padding:'5px', borderRadius:'10px'}}>
-            <div style={{width:'120',height:'32px',borderRadius:'6px', background:'grey'}}>Logo</div>
+          <Layout style={{minHeight:'100vh'}}>
+            <Sider style={{width:'200', background: colorBgContainer,padding:'10px', borderRadius:'10px'}}>
+            <div style={{width:'120',height:'32px',borderRadius:'6px', background:'lightblue', marginBottom:'4px'}}>Logo</div>
 
               <Menu
                 mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
                 style={{ height: '100%', borderRight: 0 }}
-                items={items}
-              />
+                defaultSelectedKeys={['1']}
+              >
+                {props.options.map((item : SideBarOptions ) => (
+                  <Menu.Item key={item?.key} icon={<item.icon/>}>
+                    <Link to={`/${item?.path}`}>
+                        {item?.label}
+                    </Link>
+                  </Menu.Item>
+                ))}
+              </Menu>
             </Sider>
             <Layout style={{ padding: '0 24px 0 24px' }}>
               <Breadcrumb style={{ margin: ' 0 0 12px 5px' }}>
