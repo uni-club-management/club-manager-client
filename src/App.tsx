091d2a28139462a5ae62a9313ccdec4b6aa35289
yassine <yassinemouddene@gmail.com>
2023-11-19 15:33:18 +0100

    admin all clubs list

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} fr
 import './App.css'
 import RootLayout from './layouts/RootLayout'
 import {adminOptions} from "./layouts/side-bar-options.ts";
+import ClubList from "./routes/admin/club-list";
 
 function App() {
 
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path='/' element={<RootLayout options={adminOptions}/>}>
-                <Route path='dashboard' element={<div>dashboard</div>}/>
+                <Route path='dashboard' element={<ClubList/>}/>
                 <Route path='users' element={<div>users</div>}/>
                 <Route path='team' element={<div>team</div>}/>
                 <Route path='products' element={<div>products</div>}/>
