10e2f0009ba11eabd62c9ffc134eb63cd18c4c57
yassine <yassinemouddene@gmail.com>
2023-11-19 10:52:33 +0100

    fixed sidebar and blank header

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,10 +1,9 @@
-
-import { Layout, Menu, theme } from 'antd';
+import {Layout, Menu, theme} from 'antd';
 import {Link, Outlet} from 'react-router-dom';
 import {SideBarOptions} from "./side-bar-options.ts";
 import {useState} from "react";
 
-const { Content, Sider } = Layout;
+const {Content, Sider, Header} = Layout;
 
 type Props = {
     options: SideBarOptions[]
@@ -14,42 +13,60 @@ export default function RootLayout(props: Props) {
 
     const [collapsed, setCollapsed] = useState(false);
     const {
-        token: { colorBgContainer },
-      } = theme.useToken();
-    
-      return (
-          <Layout style={{minHeight:'100vh'}}>
-            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-            <div style={{height:'32px',borderRadius:'6px', background:'rgba(255,255,255,.2)', margin:'16px'}}>Logo</div>
+        token: {colorBgContainer},
+    } = theme.useToken();
 
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                theme={"dark"}
-              >
-                {props.options.map((item : SideBarOptions ) => (
-                  <Menu.Item key={item?.key} icon={<item.icon/>}>
-                    <Link to={`/${item?.path}`}>
-                        {item?.label}
-                    </Link>
-                  </Menu.Item>
-                ))}
-              </Menu>
-            </Sider>
-            <Layout style={{ padding: '24px' }}>
-
-              <Content
+    return (
+        <Layout hasSider>
+            <Sider
                 style={{
-                  padding: 24,
-                  margin: 0,
-                  minHeight: 280,
-                  background: colorBgContainer,
+                    overflow: 'auto',
+                    height: '100vh',
+                    position: 'fixed',
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
                 }}
-              >
-                <Outlet/>
-              </Content>
+                collapsible
+                collapsed={collapsed}
+                onCollapse={(value) => setCollapsed(value)}
+            >
+                <div style={{
+                    height: '32px',
+                    borderRadius: '6px',
+                    background: 'rgba(255,255,255,.2)',
+                    margin: '16px'
+                }}>Logo
+                </div>
+
+                <Menu
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    theme={"dark"}
+                >
+                    {props.options.map((item: SideBarOptions) => (
+                        <Menu.Item key={item?.key} icon={<item.icon/>}>
+                            <Link to={`/${item?.path}`}>
+                                {item?.label}
+                            </Link>
+                        </Menu.Item>
+                    ))}
+                </Menu>
+            </Sider>
+            <Layout style={{marginLeft: collapsed ? 80 : 200, minHeight:'100vh'}}>
+                <Header style={{padding: 0, background: colorBgContainer}}/>
+                <Content
+                    style={{
+                        padding: 24,
+                        margin: 16,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                    }}
+                >
+                    <Outlet/>
+                </Content>
 
             </Layout>
-          </Layout>
-      );
+        </Layout>
+    );
 }
