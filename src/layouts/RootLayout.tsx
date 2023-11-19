e94ae6988cf9b59f533b6a8b2ed80edce4266fd8
yassine <yassinemouddene@gmail.com>
2023-11-19 10:43:42 +0100

    colapsible sidebar

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,28 +1,31 @@
 
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Layout, Menu, theme } from 'antd';
 import {Link, Outlet} from 'react-router-dom';
 import {SideBarOptions} from "./side-bar-options.ts";
+import {useState} from "react";
 
-const { Content, Sider, Footer } = Layout;
+const { Content, Sider } = Layout;
 
 type Props = {
     options: SideBarOptions[]
 }
 
 export default function RootLayout(props: Props) {
+
+    const [collapsed, setCollapsed] = useState(false);
     const {
         token: { colorBgContainer },
       } = theme.useToken();
     
       return (
           <Layout style={{minHeight:'100vh'}}>
-            <Sider style={{width:'200', background: colorBgContainer,padding:'10px', borderRadius:'10px'}}>
-            <div style={{width:'120',height:'32px',borderRadius:'6px', background:'lightblue', marginBottom:'4px'}}>Logo</div>
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+            <div style={{height:'32px',borderRadius:'6px', background:'rgba(255,255,255,.2)', margin:'16px'}}>Logo</div>
 
               <Menu
                 mode="inline"
-                style={{ height: '100%', borderRight: 0 }}
                 defaultSelectedKeys={['1']}
+                theme={"dark"}
               >
                 {props.options.map((item : SideBarOptions ) => (
                   <Menu.Item key={item?.key} icon={<item.icon/>}>
@@ -33,12 +36,8 @@ export default function RootLayout(props: Props) {
                 ))}
               </Menu>
             </Sider>
-            <Layout style={{ padding: '0 24px 0 24px' }}>
-              <Breadcrumb style={{ margin: ' 0 0 12px 5px' }}>
-                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                <Breadcrumb.Item>List</Breadcrumb.Item>
-                <Breadcrumb.Item>App</Breadcrumb.Item>
-              </Breadcrumb>
+            <Layout style={{ padding: '24px' }}>
+
               <Content
                 style={{
                   padding: 24,
@@ -49,7 +48,6 @@ export default function RootLayout(props: Props) {
               >
                 <Outlet/>
               </Content>
-            <Footer style={{ textAlign: 'center' }}>UIR ©2023 Free Palestine</Footer>
 
             </Layout>
           </Layout>
