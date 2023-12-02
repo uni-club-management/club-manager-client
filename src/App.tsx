abfaf2108cbb0241a265874c8db9724bd4206def
yassine <yassinemouddene@gmail.com>
2023-12-02 12:53:13 +0100

    simple login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
 import './App.css'
-import RootLayout from './layouts/RootLayout'
-import {adminOptions} from "./layouts/side-bar-options.ts";
+import LoginPage from "./routes/login";
+
 
 function App() {
 
     const router = createBrowserRouter(
         createRoutesFromElements(
-            <Route path='/' element={<RootLayout options={adminOptions}/>}>
-                <Route path='dashboard' element={<div>dashboard</div>}/>
-                <Route path='users' element={<div>users</div>}/>
-                <Route path='team' element={<div>team</div>}/>
-                <Route path='products' element={<div>products</div>}/>
+            <Route path='/'>
+                <Route path={"login"} element={<LoginPage/>}/>
             </Route>
         )
     )
