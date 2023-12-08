51d1931bbcc66f0a22f7a449f9fe15a0b343b877
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-12-08 22:13:58 +0100

    add status filter to filter events by eventStatus

--- src/routes/president/events/index.tsx
+++ src/routes/president/events/index.tsx
@@ -1,9 +1,10 @@
-import { Flex } from 'antd'
+import { Flex, Select } from 'antd'
 import React, { useState } from 'react'
-import { Event } from '../../../types'
+import { Event, EventStatusEnum } from '../../../types'
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
 import EventCard from './components/eventCard'
+import Search from 'antd/es/input/Search'
 
 type Props = {}
 
@@ -13,11 +14,17 @@ function events({}: Props) {
     const [pageNumber,setPageNumber]= useState<number>(1)
     const [pageSize, setPageSize] = useState<number>(25)
 
+    const [search, setSearch] = useState<string>("")
+    const [status, setStatus] = useState<string>("")
+
     const getEvents =():Promise<Event[]>=>{
         return axios.get(`http://localhost:8080/api/v1/clubs/${clubId}/events`,{
             params:{
                 pageNumber: pageNumber-1,
-                pageSize: pageSize
+                pageSize: pageSize,
+                search:search,
+                status:status
+
             }
         }).then(res =>{
             console.log("events:",res.data )
@@ -30,16 +37,26 @@ function events({}: Props) {
     }
 
     const events = useQuery<Event[],Error>({
-        queryKey:['events',clubId,pageNumber,pageSize],
+        queryKey:['events',clubId,pageNumber,pageSize,search,status],
         queryFn: getEvents
     })
 
+   
+    const onSelectStatus = (value:string)=>{
+        setStatus(value)
+    }
   return (
+    <>
+    <Flex style={{marginBottom:'20px'}} gap="large">
+        <Select size='large' placeholder="Status" onChange={e => onSelectStatus(e)} style={{width:'10rem'}} options={[{value:'',label:'ALL'},{value:'REQUESTED'},{value:'APPROVED'},{value:'REJECTED'},{value:'POST_EVENT'},{value:'CLOSED'}]} loading={events.isLoading}/>
+    </Flex>
     <Flex gap="middle" wrap='wrap'>
         {events.data?.map((event)=>(
             <EventCard event={event} loading={false}/>
         ))}
     </Flex>
+    </>
+    
   )
 }
 
