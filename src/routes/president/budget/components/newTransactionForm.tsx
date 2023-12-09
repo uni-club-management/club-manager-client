42fd79b42ca2c830b583ad0627aad7192eef9ea5
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-12-09 23:05:23 +0100

    handle form submition
    call post request and display green or red alert depending if transaction is created or not

--- src/routes/president/budget/components/newTransactionForm.tsx
+++ src/routes/president/budget/components/newTransactionForm.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Form, InputNumber, Modal, Select, Upload } from 'antd';
+import { Alert, Button, Form, InputNumber, Modal, Select, Upload } from 'antd';
 import { Budget, Event } from '../../../../types';
 import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { calculateRemainingBudget } from '../../../../utils/remaining-budget';
 import { InboxOutlined } from '@ant-design/icons';
 
@@ -21,8 +21,10 @@ const normFile = (e: any) => {
   };
 
 const newTransactionModal = ({ isVisible, setIsModalVisible, budget }: Props) => {
-
+    const [form] = Form.useForm();
     const [clubId, setClubId] = useState<number>(2)
+    const [isTransactionCreated, setIsTransactionCreated] = useState<boolean>(false)
+    const [error_transactionNotCreated, setError_transactionNotCreated] = useState<boolean>(false)
 
     const getEvents = (): Promise<Event[]>=>{
         return axios.get(`http://localhost:8080/api/v1/clubs/${clubId}/events`).then(res=>{
@@ -38,26 +40,64 @@ const newTransactionModal = ({ isVisible, setIsModalVisible, budget }: Props) =>
         queryFn: getEvents
     
     })
+    const createNewTransaction = (values: {idEvent:number,valeur:number}):Promise<any> => {       
+            return axios.post(`http://localhost:8080/api/v1/transactions`, {
+                date: new Date(),
+                valeur: values.valeur,
+                idBudget: budget.idBudget,
+                idEvent: values.idEvent,
+            })         
+    }
 
+    const mutation = useMutation({
+        mutationFn: createNewTransaction,
+        onSuccess: () => {
+            setIsTransactionCreated(true)
+            setTimeout(()=>setIsTransactionCreated(false),5000)
+            form.resetFields()
+            console.log('transaction created')
+        },
+        onError: (error) => {
+            setError_transactionNotCreated(true)
+            // setTimeout(()=>setError_transactionNotCreated(false),3000)
+            console.log('transaction not created', error)
+        }
+    })
+    
+
+   
     return (
-        <Modal title="Request a Transaction" open={isVisible} onCancel={() => setIsModalVisible(false)}>
-            <Form labelCol={{span:4}}  style={{paddingTop:'2em'}}>
-                <Form.Item label="Source">
+        <Modal 
+            title="Request a Transaction" 
+            open={isVisible} 
+            onCancel={() => setIsModalVisible(false)} 
+            
+            footer={[
+                <Button key='submit' type='primary' 
+                    loading={mutation.isPending} 
+                    onClick={()=> form.submit()}>submit</Button>,
+                <Button key='cancel'  onClick={()=>setIsModalVisible(false)}>cancel</Button>
+                ]}
+        >
+            {isTransactionCreated &&<Alert closable message='Transaction requested successfully' type='success'  />}
+            {error_transactionNotCreated &&<Alert closable message={`Couldn't request transaction : ${mutation.error?.message}`}  type='error'  />}
+            <Form onFinish={mutation.mutate} form={form} labelCol={{span:4}}  style={{paddingTop:'2em'}}>
+                <Form.Item label="Source" >
                     <b>Budget {budget.budgetType}</b>
                 </Form.Item>
-                <Form.Item label="Event" name="event">
+                <Form.Item label="Event" name="idEvent">
                     <Select style={{width:'10rem'}}>
                         {events.data?.map((event)=>(
                             <Select.Option value={event.idEvent}>{event.name}</Select.Option>
                         ))}
                     </Select>
                 </Form.Item>
-                <Form.Item label="Amount">
+                <Form.Item label="Amount" name="valeur">
                     <InputNumber min={0} max={calculateRemainingBudget(budget)}/>
                 </Form.Item>
                 <Form.Item label="Documents">
-                <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
-                    <Upload.Dragger name="files" action="/upload.do">
+                <Form.Item /*name="dragger"*/ valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+                    <Upload.Dragger /*name="files" action="/upload.do"*/>
                     <p className="ant-upload-drag-icon">
                         <InboxOutlined />
                     </p>
