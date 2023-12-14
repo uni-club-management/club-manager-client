f98ba6f64486543d0299851fc9e49e6fec163c6e
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-12-14 23:07:18 +0100

    feat: add delete pending transaction query and apply it in action

--- src/routes/president/budget/components/budgetTransactions.tsx
+++ src/routes/president/budget/components/budgetTransactions.tsx
@@ -1,56 +1,13 @@
-import { Button, Card, Form, Modal, Table, Tag } from 'antd'
+import { Button, Card, Form, Modal, Popconfirm, Table, Tag } from 'antd'
 import React, { useState } from 'react'
 import { Budget, Event, Transaction } from '../../../../types'
 import axios from 'axios'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { ColumnsType } from 'antd/es/table'
-import { EditOutlined, PlusOutlined } from '@ant-design/icons'
+import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons'
 import NewTransactionModal from './newTransactionForm'
 
-const columns : ColumnsType<Transaction> = [
-    {
-    title: 'Event',
-    dataIndex: 'event',
-    key: 'event',
-    render: (event : Event)=> event.name
-    },
-    
-    {
-        title: 'amount',
-        dataIndex: 'valeur',
-        key: 'amount'
-    },
-    
-    {
-        title: 'Documents',
-        dataIndex: 'preuve',
-        key: 'docs'
-    },
-    {
-        title: 'Date',
-        dataIndex: 'date',
-        key: 'date',
-        render: (date: Date) => new Date(date).toLocaleDateString()
-    },
-    {
-        title:'Status',
-        dataIndex:'status',
-        key:'status',
-        render: (status : string)=> (
-            <Tag color={status=='APPROVED'?'green':status=='REJECTED'?'red':status=='PENDING'?'blue':'default'}>{status}</Tag>
-        )
-    },
-    {
-        title:'Action',
-        dataIndex:'status',
-        key:'action',
-        width: 100,
-        render: (status)=>(
-            <Button type='link' disabled={status!='PENDING'}><EditOutlined/></Button>
-        )
-    }
-   
-]
+
 
 type Props = {
     budget: Budget
@@ -81,11 +38,70 @@ const budgetTransactions = ({budget}: Props) => {
     }
 
     
-    const transactions = useQuery<Transaction[],Error>({
-        queryKey:['transactions',budget.idBudget,pageNumber,pageSize],
-        queryFn: getTransactions
-    })
+        const transactions = useQuery<Transaction[],Error>({
+            queryKey:['transactions',budget.idBudget,pageNumber,pageSize],
+            queryFn: getTransactions
+        })
+       
+        const deleteTransaction = useMutation({
+            mutationFn: (id:number|undefined):Promise<any> =>{
+                return axios.delete(`http://localhost:8080/api/v1/transactions/${id}`)
+            },
+            onSuccess: ()=>{
+                console.log('transaction deleted')
+                transactions.refetch()
+            },
+            onError: (error)=>{
+                console.error('transaction not deleted',error)
+            }
+        })
 
+        const columns : ColumnsType<Transaction> = [
+            {
+            title: 'Event',
+            dataIndex: 'event',
+            key: 'event',
+            render: (event : Event)=> event.name
+            },
+            
+            {
+                title: 'amount',
+                dataIndex: 'valeur',
+                key: 'amount'
+            },
+            
+            {
+                title: 'Documents',
+                dataIndex: 'preuve',
+                key: 'docs'
+            },
+            {
+                title: 'Date',
+                dataIndex: 'date',
+                key: 'date',
+                render: (date: Date) => new Date(date).toLocaleDateString()
+            },
+            {
+                title:'Status',
+                dataIndex:'status',
+                key:'status',
+                render: (status : string)=> (
+                    <Tag color={status=='APPROVED'?'green':status=='REJECTED'?'red':status=='PENDING'?'blue':'default'}>{status}</Tag>
+                )
+            },
+            {
+                title:'Action',
+                dataIndex:'status',
+                key:'action',
+                width: 100,
+                render: (status,record)=>(
+                    <Popconfirm title='Sure to delete ?' okText='Yes' cancelText='No' onConfirm={()=>{deleteTransaction.mutate(record.idTransaction)}} >
+                        <Button type='link' disabled={status!='PENDING'}><DeleteOutlined/></Button>
+                    </Popconfirm>
+                )
+            }
+           
+        ]
   return (
     <Card title='Transactions History' style={{width:'100%'}} extra={<Button onClick={e=>setIsModalVisible(true)} type='primary' icon={<PlusOutlined/>}>New Transaction</Button>} >
         <Table
