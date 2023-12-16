c1b3bd0d205c4a1011d7f5e6dbeeb2098fe06250
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-12-16 17:13:17 +0100

    style: set height for cover image in eventCard and apply space between event status and description

--- src/routes/president/events/components/eventCard.tsx
+++ src/routes/president/events/components/eventCard.tsx
@@ -31,11 +31,13 @@ const App: React.FC<EventCardProp> = ({event, loading}) => {
       loading={loading}
       hoverable
       style={{ width: 240 }}
-      cover={<img alt="Event-cover" src={event.cover}  />}
+      cover={<img alt="Event-cover" src={event.cover} height={160} />}
       onClick= {handleClick}
     > 
+      <Flex vertical justify='space-between' align='flex-start' style={{height:'120px'}}>
         <Meta title={event.name} description={event.description} key={event.idEvent}  />
         <Tag color={statusColors[event.status as keyof typeof statusColors]}>{event.status}</Tag>
+      </Flex>
       
     </Card>
     
