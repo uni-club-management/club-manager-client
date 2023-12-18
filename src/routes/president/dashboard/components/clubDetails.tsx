5c18484a882447850f4aa76ba5ca3d8c569f2c7a
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2023-12-18 11:52:25 +0100

    feat: get club details

---
+++ src/routes/president/dashboard/components/clubDetails.tsx
@@ -0,0 +1,14 @@
+import { Card } from 'antd'
+import React from 'react'
+
+type Props = {}
+
+function ClubDetails({}: Props) {
+  return (
+    <Card >
+
+    </Card>
+  )
+}
+
+export default ClubDetails
--- src/routes/president/dashboard/index.tsx
+++ src/routes/president/dashboard/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import {ClubContext} from "../../../context";
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { ClubDetails } from '../../../types';
+import axios from 'axios';
+import { Card, Flex, Image } from 'antd';
 
 type Props = {}
 
 const Dashboard = (props: Props) => {
+    const {clubId} = useParams();
+    const club = React.useContext(ClubContext)
+    const idClub = (clubId && +clubId) ?? club.clubId ?? 0
+
+    const getClubDetails = ()=>{
+        
+    }
+    const clubDetails = useQuery<ClubDetails,Error>({
+        queryKey:["clubDetails",idClub],
+        queryFn:()=>axios.get(`http://localhost:8080/api/v1/clubs/${idClub}/details`),
+        
+    }
+    )
+    
+
   return (
-    <div>Dashboard</div>
+    <Card>
+        <Flex>
+            <Image src={clubDetails.data?.cover}/>
+            
+        </Flex>
+    </Card>
   )
 }
 
