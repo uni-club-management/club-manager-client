0ba434137986a2ad6177cd38018b1e0cd1e50c39
Hamza El mesaouab <77578145+Hamza-MS@users.noreply.github.com>
2024-01-09 21:57:36 +0100

    add barChart to dashboard

--- src/routes/president/dashboard/index.tsx
+++ src/routes/president/dashboard/index.tsx
@@ -5,9 +5,11 @@ import { useQuery } from "@tanstack/react-query";
 import { ClubDetails } from "../../../types";
 import axios from "axios";
 import {
+  Card,
   Flex,
 } from "antd";
 import ClubDetailsCard from "./components/clubDetails";
+import BarChart from "./components/barChart";
 
 type Props = {};
 
@@ -15,6 +17,8 @@ const Dashboard = (props: Props) => {
   const { clubId } = useParams();
   const club = React.useContext(ClubContext);
   const idClub = (clubId && +clubId) ?? club.clubId ?? 0;
+  const isMobile :boolean = window.innerWidth < 768;
+
 
   const clubDetails = useQuery<ClubDetails, Error>({
     queryKey: ["clubDetails", idClub],
@@ -23,10 +27,16 @@ const Dashboard = (props: Props) => {
         .get(`http://localhost:8080/api/v1/clubs/${2}/details`)
         .then((res) => res.data),
   });
-  const isMobile :boolean = window.innerWidth < 768;
+
+  
+
   return (
-    <Flex vertical gap={"16px"}>
+    <Flex vertical gap="middle" >
       <ClubDetailsCard clubDetails={clubDetails} isMobile={isMobile} />
+
+      <Flex wrap="wrap">
+          <BarChart idClub={idClub }/>
+      </Flex>
     </Flex>
 
   );
