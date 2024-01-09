2de5df1d134ee8bf6ffcc59ff911f3c548945472
yassine <yassinemouddene@gmail.com>
2024-01-09 22:38:36 +0100

    feature: file upload and download for club members

--- src/routes/president/members/components/membersTable.tsx
+++ src/routes/president/members/components/membersTable.tsx
@@ -1,13 +1,14 @@
-import {Card, Table} from 'antd'
+import {Card, Flex, Table, Tooltip} from 'antd'
 import {ColumnsType} from 'antd/es/table';
-import { useState} from 'react'
-import { Student} from '../../../../types';
+import {useContext, useRef, useState} from 'react'
+import {Student} from '../../../../types';
 import axios from "axios"
-import {useQuery} from "@tanstack/react-query";
-import {ExportOutlined} from '@ant-design/icons';
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+import {ExportOutlined, UploadOutlined} from '@ant-design/icons';
+import {ClubContext} from "../../../../context";
+import {toast} from "react-toastify";
 
-//TODO: implement card's export action
-//TODO: get club id dynamicly
+//TODO: add ts types
 
 const columns: ColumnsType<Student> = [{
     title: 'First Name',
@@ -38,17 +39,66 @@ const columns: ColumnsType<Student> = [{
 ]
 
 
-type Props = {
-    idClub: number
-}
-
-export default function MembersTable(props: Props) {
+export default function MembersTable() {
     const [pageNumber, setPageNumber] = useState<number>(1)
-    const [pageSize, setPageSize] = useState<number>(3)
+    const [pageSize, setPageSize] = useState<number>(10)
     const [totalRows, setTotalRows] = useState<number>(30)
+    const {clubId} = useContext(ClubContext);
+    const queryClient = useQueryClient()
+
+    // I know I should use "any" but I don't know how to fix it
+    const hiddenFileInput = useRef<any>(null); // ADDED
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const handleClick = event => {
+        hiddenFileInput!.current!.click();
+    };
+
+    const handleChange = event => {
+        const fileUploaded = event.target.files[0];
+        mutation.mutate(fileUploaded);
+    };
+
+
+    const mutation = useMutation({
+        mutationFn: (file: File) => {
+            return axios.postForm(`http://localhost:8080/api/v1/clubs/${clubId}/members`, {
+                "file": file
+            }, {
+                onUploadProgress: progressEvent => console.log((progressEvent.loaded / (progressEvent.total ?? 1)) * 100)
+            })
+        },
+        onSuccess: () => {
+
+            queryClient.refetchQueries({queryKey: ['members', pageNumber, pageSize]}).then(() => {
+                toast.success('file uploaded successfully', {
+                    position: "bottom-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                });
+            })
+        },
+        onError: () => {
+            toast.error('A problem occurred', {
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+        },
+    })
 
     const getClubMembers = (): Promise<Student[]> => {
-        return axios.get<Student[]>(`http://localhost:8080/api/v1/clubs/${props.idClub}/members`, {
+        return axios.get<Student[]>(`http://localhost:8080/api/v1/clubs/${clubId}/members`, {
                 params: {
                     pageNumber: pageNumber - 1,
                     pageSize: pageSize,
@@ -70,10 +120,43 @@ export default function MembersTable(props: Props) {
         queryFn: getClubMembers,
     })
 
+    const download = () => {
+        axios.get(`http://localhost:8080/api/v1/clubs/${clubId}/members/file`, {responseType: 'blob'}).then(response => {
+            const type = response.headers['content-type']
+            const blob = new Blob([response.data], {type: type})
+            const link = document.createElement('a')
+            link.href = window.URL.createObjectURL(blob)
+            link.download = "member list.xlsx"
+            link.click()
+        }).catch((response) => {
+            console.error("Could not Download the Excel report from the backend.", response);
+        });
+    }
+
+
     return (
         <Card
             title="All Members"
-            extra={<ExportOutlined/>}
+            extra={
+                <Flex align={"center"} gap={24}>
+                    <Tooltip title={"import"}>
+                        <>
+                            <UploadOutlined onClick={handleClick}/>
+                            <input
+                                onChange={handleChange}
+                                ref={hiddenFileInput}
+                                type="file"
+                                id="upload"
+                                style={{display: "none"}}/>
+                        </>
+
+
+                    </Tooltip>
+                    <Tooltip title={"export"}>
+                        <ExportOutlined onClick={() => download()}/>
+                    </Tooltip>
+                </Flex>
+            }
             style={{marginTop: '20px'}}
         >
             <Table
